512200d46c16e63b3904c761f83120b6dfc90b08
Enzo <enzoboitte63000@gmail.com>
2025-03-28 14:49:36 +0100

    Added login

--- index.php
+++ index.php
@@ -7,8 +7,10 @@ require_once "$G_sRacine/model/Routes.php";
 // route pour le dashboard
 route('/{}', 'GET', function ($link) 
 {
+    global $G_sRacine;
+    include_once "$G_sRacine/model/Account.php";
+    //(new CAccount())->F_vDieSession();
     F_vRequest($link);
-    
 });
 
 function F_vRequest(string $link)
@@ -17,11 +19,36 @@ function F_vRequest(string $link)
     global $G_sRacine, $G_sAction, $G_sLayout;
     
     $G_sAction  = isset($link) && $link != "" ? $link : "default";
-    include "$G_sRacine/controler/ctlMain.php";
-    include "$G_sRacine/view/$G_sLayout";
+    include_once "$G_sRacine/controler/ctlMain.php";
+    include_once "$G_sRacine/view/$G_sLayout";
     if($G_sLayout != "layout.php")
         $G_sLayout = "layout.php";
 }
+
+// route pour les requêtes API
+
+# /v1/account/login/{username},{password}
+route('/v1/account/login/{},{}', 'GET', function ($l_sIdentifier, $l_sPasswd) 
+{
+    global $G_sRacine;
+    include_once "$G_sRacine/model/Account.php";
+
+    $res = (new CAccount())->F_iLogin($l_sIdentifier, $l_sPasswd);
+    $msg = "";
+
+    switch($res)
+    {
+        case 0: $msg = $_SESSION["tmpkey"]; break;
+        case 1: $msg = "Identifiant incorrect."; break;
+        case 2: $msg = "Mot de passe incorrect."; break;
+        default: $msg = "internal error."; break;
+    }
+
+    echo json_encode([
+            "code" => $res,
+            "msg" => $msg
+        ]);
+});
 
 
 
--- model/param.php
+++ model/param.php
@@ -10,6 +10,7 @@ $G_sLayout = "layout.php";
 $G_lActions = [
     "default"     => "home",
     "home"        => "home",
+    "dashboard"   => "dashboard",
 
     "404"       => "404",
     "405"       => "405"
