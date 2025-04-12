7cd2d4e6a860adf31aa43fec838449eddb13c6c5
TASKIN <staskin358@gmail.com>
2025-04-12 18:54:08 +0200

    testetste

--- model/CMembers.php
+++ model/CMembers.php
@@ -2,17 +2,19 @@
 
 include_once "$G_sRacine/model/PDOModel.php";
 
-class CMembers  extends CPDOModel
+class CMembers extends CPDOModel
 {
-    public function getMembersByYear($year) {
+    public function getMembersByYear($libelleAnnee) {
         $pdo = $this->F_cGetDB();
         $stmt = $pdo->prepare("
             SELECT m.*, r.libelle AS role
             FROM membre m
-            LEFT JOIN role r ON m.idRo = r.idRo
-            WHERE m.annee = :annee
+            INNER JOIN nommer n ON m.idM = n.idM
+            INNER JOIN annee a ON a.idA = n.idA
+            INNER JOIN role r ON n.idRo = r.idRo
+            WHERE a.libelle = :annee
         ");
-        $stmt->bindParam(':annee', $year);
+        $stmt->bindParam(':annee', $libelleAnnee);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
